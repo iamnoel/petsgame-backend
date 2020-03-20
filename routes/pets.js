c622cd9bea89b4aaf6bab2459e459a884e9ace58
Noel <noel.ward@icloud.com>
2020-03-19 21:32:25 -0400

    adjusted delete to use deleteOne instead of remove

--- routes/pets.js
+++ routes/pets.js
@@ -83,7 +83,7 @@ router.patch('/:petID', (req, res, next) => {
 
 router.delete('/:petID', (req, res, next) => {
   const id = req.params.petID;
-  Pet.remove({_id: id})
+  Pet.deleteOne({_id: id})
       .exec()
       .then( (result) => {
         res.status(200).json({
