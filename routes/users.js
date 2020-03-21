02665c121728c66bf33343f2e3466e75ede37bc1
Noel <noel.ward@icloud.com>
2020-03-21 15:50:46 -0400

    create new user account

--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const User = require('../models/users');
 
 router.get('/', (req, res, next) => {
   res.status(200).json({
     message: 'Users were fetched',
-  });
-});
-
-router.post('/', (req, res, next) => {
-  const user = {
-    name: req.body.name,
-    id: req.body.id,
-  };
-  res.status(201).json({
-    message: 'User was created',
-    user: user,
   });
 });
 
@@ -23,6 +16,31 @@ router.get('/:userID', (req, res, next) => {
     message: 'User details',
     userID: req.params.userID,
   });
+});
+
+router.post('/', async (req, res, next) => {
+  try {
+    hashedPass = await bcrypt.hash(req.body.password, 10);
+    const user = new User({
+      _id: new mongoose.Types.ObjectId(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPass,
+    });
+    const addUser = await user.save();
+    if (addUser) {
+      res.status(201).json({
+        message: 'User was created',
+        user: addUser,
+      });
+    }
+  } catch (error) {
+    console.log('Error: ', error);
+    res.status(500).json({
+      message: `An error was encountered trying to user pet: ${user.name}`,
+      error: error,
+    });
+  }
 });
 
 router.delete('/:userID', (req, res, next) => {
