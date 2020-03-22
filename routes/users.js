553aeb0e9cff5d8cf62303c2bc99e3445c615967
Noel <noel.ward@icloud.com>
2020-03-21 22:15:23 -0400

    disabled passport

--- routes/users.js
+++ routes/users.js
@@ -3,29 +3,29 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
-const passport = require('passport');
-const flash = require('express-flash');
-const session = require('express-session');
+// const passport = require('passport');
+// const flash = require('express-flash');
+// const session = require('express-session');
 
-const initializePassport = require('../passport-config');
-initializePassport(passport,
-    (email) => userz.find((user) => user.email == email),
-    (id) => userz.find((user) => user._id == id),
-);
+// const initializePassport = require('../passport-config');
+// initializePassport(passport,
+//     (email) => userz.find((user) => user.email == email),
+//     (id) => userz.find((user) => user._id == id),
+// );
 
 const userz = [];
 
 const User = require('../models/users');
 
-router.use(flash());
-router.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false,
-}));
+// router.use(flash());
+// router.use(session({
+//   secret: process.env.SESSION_SECRET,
+//   resave: false,
+//   saveUninitialized: false,
+// }));
 
-router.use(passport.initialize());
-router.use(passport.session());
+// router.use(passport.initialize());
+// router.use(passport.session());
 
 router.get('/', async (req, res, next) => {
   try {
@@ -84,12 +84,12 @@ const getUserByID = async (id) => {
   }
 };
 
-router.post('/login', passport.authenticate('local'), (req, res, next)=> {
-  res.status(200).json({
-    message: 'USER',
-    user: req.body.name,
-  });
-});
+// router.post('/login', passport.authenticate('local'), (req, res, next)=> {
+//   res.status(200).json({
+//     message: 'USER',
+//     user: req.body.name,
+//   });
+// });
 
 router.post('/', async (req, res, next) => {
   try {
