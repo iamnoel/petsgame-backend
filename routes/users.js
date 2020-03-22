b71bae43b236d34f74c354da366376dcf6bdcbef
Noel <noel.ward@icloud.com>
2020-03-22 00:28:05 -0400

    import bcrypt

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-// const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 // const passport = require('passport');
 // const flash = require('express-flash');
 // const session = require('express-session');
