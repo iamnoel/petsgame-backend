5f6c5afff0c6558e518aea5b4abe0af95457565c
Noel <noel.ward@icloud.com>
2020-03-21 21:50:17 -0400

    CORS allow credentials

--- app.js
+++ app.js
@@ -21,7 +21,9 @@ app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
   if (req.method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
     return res.status(200).json({});
