0cc20fcec59c677f7f5824a6c6735651bd5fc51e
Noel <noel.ward@icloud.com>
2020-03-21 20:50:18 -0400

    Added login

--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,72 @@
+require('dotenv').config();
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const passport = require('passport');
+const flash = require('express-flash');
+const session = require('express-session');
+
+const initializePassport = require('../passport-config');
+initializePassport(passport,
+    (email) => userz.find((user) => user.email == email),
+    (id) => userz.find((user) => user._id == id),
+);
+
+const userz = [];
 
 const User = require('../models/users');
 
-router.get('/', (req, res, next) => {
-  res.status(200).json({
-    message: 'Users were fetched',
-  });
+router.use(flash());
+router.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+}));
+
+router.use(passport.initialize());
+router.use(passport.session());
+
+router.get('/', async (req, res, next) => {
+  try {
+    const users = await User.find();
+    console.log(users);
+    res.status(200).json({users});
+  } catch (error) {
+    console.log('Error', error);
+    res.status(500).json({
+      message: 'An error was encountered trying to get all users',
+      error: error,
+    });
+  }
 });
 
-router.get('/:userID', (req, res, next) => {
+router.get('/:userID', async (req, res, next) => {
+  try {
+    const id = req.params.petID;
+    const user = await User.findById(id);
+    console.log(`Received from the db: ${user}`);
+    if (user) {
+      res.status(200).json({user});
+    } else {
+      res.status(404).json({
+        message: `There were 0 results returned for id: ${id}`,
+        user,
+      });
+    }
+  } catch (error) {
+    console.log(`An error was encountered trying to get pet id: ${id}`);
+    res.status(500).json({
+      message: `An error was encountered trying to get pet id: ${id}`,
+      error: error,
+    });
+  }
+});
+
+router.post('/login', passport.authenticate('local'), (req, res, next)=> {
   res.status(200).json({
-    message: 'User details',
-    userID: req.params.userID,
+    message: 'USER',
+    user: req.body.name,
   });
 });
 
@@ -27,7 +79,10 @@ router.post('/', async (req, res, next) => {
       email: req.body.email,
       password: hashedPass,
     });
+    userz.push(user);
+    console.log(user);
     const addUser = await user.save();
+    console.log(userz);
     if (addUser) {
       res.status(201).json({
         message: 'User was created',
